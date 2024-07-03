9c0484c2a8c0320e024ee6618283226ce25f20d0
Lius <morarolivio@gmail.com>
2024-07-03 23:37:13 +0200

    finish App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,70 @@
-function App() {
+import React, {useEffect, useState} from 'react';
+// import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
+import {Menu, Spin} from 'antd';
+import axios from "axios";
+import CryptocurrencyCard from "./components/CryptocurrencyCard.jsx";
+// import * as r from "autoprefixer";
 
+
+const App = () => {
+    const [currencies, setCurrencies] = useState([])
+    const [currencyId, setCurrencyId] = useState(1)
+    const [currencyData, setCurrencyData] = useState(null)
+
+    const fetchCurrencies = () => {
+        axios.get("http://127.0.0.1:8000/cryptocurrencies/").then((response) => {
+            const currenciesResponse = response.data
+            const menuItems = [
+                {
+                    key: 'g1',
+                    label: 'List cryptocurrency',
+                    type: 'group',
+                    children: currenciesResponse.map(c => ({label: c.name, key: c.id}))
+
+                }
+            ]
+            setCurrencies(menuItems)
+            console.log(currenciesResponse)
+        })
+    }
+
+    const fetchCurrency = () => {
+        axios.get(`http://127.0.0.1:8000/cryptocurrencies/${currencyId}`).then((response) => {
+            setCurrencyData(response.data)
+            console.log(response.data)
+        })
+    }
+
+    useEffect(() => {
+        fetchCurrencies()
+    }, []);
+
+    useEffect(() => {
+        setCurrencyData(null)
+        fetchCurrency()
+    }, [currencyId]);
+
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrencyId(e.key)
+    };
     return (
-        <>
-            <h1 className="text-3xl  underline">
-                Hello world!
-            </h1>
-        </>
-    )
-}
-
-export default App
+        <div className="flex">
+            <Menu
+                onClick={onClick}
+                style={{
+                    width: 256,
+                }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                items={currencies}
+                className="h-screen overflow-scroll"
+            />
+            <div className="mx-auto my-auto">
+                {currencyData ? <CryptocurrencyCard currency={currencyData}/> : <Spin size="large"/>}
+            </div>
+        </div>
+    );
+};
+export default App;
